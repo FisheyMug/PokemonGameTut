250fe593af477ecfebe297cebf0d8ea54d8d025f
Matthew <96601521+FisheyMug@users.noreply.github.com>
2023-02-04 21:18:05 +0900

    added data file with collision map, tiled map and followed further in the tutorial

--- script.js
+++ script.js
@@ -4,24 +4,97 @@ const ctx = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 576
 
+console.log(collisions);
+
+const collisionsMap = []
+for (let i = 0; i < collisions.length; i+=70) {
+    collisionsMap.push(collisions.slice(i, 70 + i))
+}
+
+const offset = {
+    x: -350,
+    y: -750
+}
+
+class Boundary {
+    static width = 48
+    static height = 48
+    constructor(position) {
+        this.position = position
+        this.width = 48
+        this.height = 48
+    }
+
+    draw() {
+        ctx.fillStyle = "red"
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+const boundaries = []
+
+collisionsMap.forEach ((row, i) => {
+    row.forEach((symbol, j) => {
+        if (symbol = 1025) {
+            boundaries.push(new Boundary({position: {
+                x:j * Boundary.width + offset.x,
+                y:i * Boundary.height + offset.y
+            }}))
+        }
+       
+    })
+})
+
 const map = new Image()
 map.src = './img/fishTown.png'
 const playerImage = new Image()
 playerImage.src = './img/playerDown.png'
 
 class Sprite {
-    constructor({
-        position,
-        velocity
-    }) {
+    constructor({position, velocity, image}) {
         this.position = position
+        this.image = image
+    }
+
+    draw () {
+        ctx.drawImage(this.image, this.position.x, this.position.y)
     }
 }
 
 
+
+const background = new Sprite({
+    position: {
+        x: offset.x,
+        y: offset.y
+    },
+    image: map
+})
+
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+}
+let lastKey = '';
+
+
 function animate() {
     requestAnimationFrame(animate)
-    ctx.drawImage(map, -350, -750)
+    background.draw()
+    boundaries.forEach( boundary => {
+        console.log('draw')
+        boundary.draw()
+    })
     ctx.drawImage(playerImage, 
         0,
         0,
@@ -30,25 +103,58 @@ function animate() {
         canvas.width / 2 - (playerImage.width /4) / 2,
         canvas.height /2 - playerImage.height / 2,
         playerImage.width/4,
-        playerImage.height)
+        playerImage.height
+        )
+    
+    
+    if (keys.w.pressed && lastKey === 'w') background.position.y += 3
+    else if (keys.a.pressed && lastKey === 'a') background.position.x += 3
+    else if (keys.s.pressed && lastKey === 's') background.position.y -= 3
+    else if (keys.d.pressed && lastKey === 'd') background.position.x -= 3
+
+
+    
 }
 animate()
 
-// addEventListener('keydown', ({key}) => {
-//     console.log(key)
-//     switch (key) {
-//         case: 'w':
+addEventListener('keydown', ({key}) => {
 
-//         break
-//         case: 'a':
+    switch (key) {
+        case 'w':
+            keys.w.pressed = true
+            lastKey = 'w'
+        break
+        case 'a':
+            keys.a.pressed = true
+            lastKey = 'a'
+        break
+        case 's':
+            keys.s.pressed = true
+            lastKey = 's'
+        break
+        case 'd':
+            keys.d.pressed = true
+            lastKey = 'd'
+        break
 
-//         break
-//         case: 's':
+    }
+})
 
-//         break
-//         case: 'd':
+addEventListener('keyup', ({key}) => {
+    
+    switch (key) {
+        case 'w':
+            keys.w.pressed = false
+            break
+        case 'a':
+            keys.a.pressed = false
+            break
+        case 's':
+            keys.s.pressed = false
+            break
+        case 'd':
+            keys.d.pressed = false
+            break
 
-//         break
-
-//     }
-// })
+    }
+})
